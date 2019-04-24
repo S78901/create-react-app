d2f7d541706a178598c21f55ca57e25af27fe8a5
S78901 <sydwelldavin@gmail.com>
2019-04-24 10:32:05 -0500

    Updates

---
+++ precache-manifest.f1c12e0546c36033358383985b528c67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a2c4c0d85cc1b2a568f06569eed2629",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "03134f65ab7853524e1c",
+    "url": "/my-app/static/css/main.c5493ba5.chunk.css"
+  },
+  {
+    "revision": "aad00edffd37712551bb",
+    "url": "/my-app/static/js/2.4a10b725.chunk.js"
+  },
+  {
+    "revision": "03134f65ab7853524e1c",
+    "url": "/my-app/static/js/main.a111ee9b.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
